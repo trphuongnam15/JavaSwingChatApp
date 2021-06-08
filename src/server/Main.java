91c0bb46e4e66b940dcc669a6b7b770342647f7c
20424108 <trphuongnam15@outlook.com>
2021-06-08 14:57:36 +0700

    refactor

---
+++ src/server/Main.java
@@ -0,0 +1,10 @@
+package server;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new Server().setVisible(true);
+    }
+}
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,3 +1,5 @@
+package server;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -5,18 +7,16 @@ public class Server extends JFrame {
     JPanel p1;
 
     Server() {
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("D:\\Resources\\Personal\\JavaSwingChatApp\\icons3.png"));
         Image i2 = i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
-
+        ImageIcon i3 = new ImageIcon(i2);
         JLabel l1 = new JLabel(i1);
-        l1.setBounds(5, 5, 10, 10);
+        l1.setBounds(5, 5, 30, 30);
         setLayout(null);
         setSize(450, 700);
         setLocation(400, 200);
         setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new Server().setVisible(true);
-    }
+
 }
