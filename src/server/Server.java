0b1409228c7aab71eedfe2748d4cc7afe1624438
20424108 <trphuongnam15@outlook.com>
2021-06-08 15:19:20 +0700

    Update Server.java

--- src/server/Server.java
+++ src/server/Server.java
@@ -7,12 +7,26 @@ public class Server extends JFrame {
     JPanel p1;
 
     Server() {
+        p1 = new JPanel();
+        p1.setLayout(null);
+        p1.setBackground(new Color(7, 94, 84));
+        p1.setBounds(0, 0, 450, 70);
+        add(p1);
+
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("server/icons/3.png"));
-        Image i2 = i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
-        JLabel l1 = new JLabel(i1);
+        JLabel l1 = new JLabel(i3);
         l1.setBounds(5, 17, 30, 30);
-        add(l1);
+        p1.add(l1);
+
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("server/icons/1.png"));
+        Image i5 = i4.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel l2 = new JLabel(i6);
+        l2.setBounds(5, 17, 60, 60);
+        p1.add(l2);
+
         getContentPane().setBackground(Color.YELLOW);
         setLayout(null);
         setSize(450, 700);
