fa975c7aaf8fe0cdb09b0b16df9ab209ba7e39ab
20424108 <trphuongnam15@outlook.com>
2021-06-08 15:37:16 +0700

    Update Server.java

--- src/server/Server.java
+++ src/server/Server.java
@@ -14,7 +14,7 @@ public class Server extends JFrame {
         add(p1);
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("server/icons/3.png"));
-        Image i2 = i1.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l1 = new JLabel(i3);
         l1.setBounds(5, 17, 30, 30);
@@ -24,8 +24,20 @@ public class Server extends JFrame {
         Image i5 = i4.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
         ImageIcon i6 = new ImageIcon(i5);
         JLabel l2 = new JLabel(i6);
-        l2.setBounds(5, 17, 60, 60);
+        l2.setBounds(40, 5, 60, 60);
         p1.add(l2);
+
+        JLabel l3 = new JLabel("Gaitonde");
+        l3.setFont(new Font("SAN_SERIF", Font.BOLD, 19));
+        l3.setForeground(Color.WHITE);
+        l3.setBounds(110, 15, 100, 20);
+        p1.add(l3);
+
+        JLabel l4 = new JLabel("Active Now");
+        l4.setFont(new Font("SAN_SERIF", Font.PLAIN, 14));
+        l4.setForeground(Color.WHITE);
+        l4.setBounds(110, 35, 100, 20);
+        p1.add(l4);
 
         getContentPane().setBackground(Color.YELLOW);
         setLayout(null);
