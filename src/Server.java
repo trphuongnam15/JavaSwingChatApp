530abe185ca4094c7464ea9e8c164122f0b2ea24
20424108 <trphuongnam15@outlook.com>
2021-06-08 14:44:57 +0700

    add icons

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Server extends JFrame {
+    JPanel p1;
+
     Server() {
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+        Image i2 = i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+
+        JLabel l1 = new JLabel(i1);
+        l1.setBounds(5, 5, 10, 10);
+        setLayout(null);
         setSize(450, 700);
         setLocation(400, 200);
         setVisible(true);
