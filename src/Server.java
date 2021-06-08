ceea956e35cd478c0f27161e08bfe334c72f75d1
20424108 <trphuongnam15@outlook.com>
2021-06-08 14:05:12 +0700

    add scaffolds

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,12 @@
 import javax.swing.*;
 
 public class Server extends JFrame {
+    Server() {
+        setSize(500, 600);
+        setVisible(true);
+    }
 
+    public static void main(String[] args) {
+        new Server().setVisible(true);
+    }
 }
