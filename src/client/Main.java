9f51e720b6a037efa4d2f0592b4bc91bb6322b3c
20424108 <trphuongnam15@outlook.com>
2021-06-10 20:54:52 +0700

    Update Main.java

--- src/client/Main.java
+++ src/client/Main.java
@@ -45,7 +45,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String ipAddress = null;
+        String ipAddress;
         if(args.length != 0) {
             ipAddress = args[0];
         } else {
